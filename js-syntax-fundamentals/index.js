bf20c5de1ead5b4e72d27de017f63fc949bbc4a5
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2023-06-21 20:44:33 +0200

    Add files via upload

---
+++ js-syntax-fundamentals/index.js
@@ -0,0 +1,5 @@
+function multiplayByTwo(a) {
+    console.log(a*2)
+}
+
+multiplayByTwo(2)
---
+++ js-syntax-fundamentals/student-information.js
@@ -0,0 +1,5 @@
+function printStudentDEtails(name, age, averageGrade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${averageGrade.toFixed(2)}`)
+}
+
+printStudentDEtails("Alex", 21, 2.213123)
