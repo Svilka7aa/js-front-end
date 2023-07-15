9fd21e7a0b2a46c1cfd6ff8e33299376d067905c
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2023-07-15 14:43:05 +0200

    Add files via upload

--- DOM-and-events/01. Sum Numbers_Ресурси/04. List-Of-Items/app.js
+++ DOM-and-events/01. Sum Numbers_Ресурси/04. List-Of-Items/app.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    // console.log('TODO:...');
+    const value = document.querySelector("#newItemText").value;
+    
+    // document.querySelector("ul").innerHTML += `<li>${value}</li>`
+
+    const item = document.createElement("li");
+    item.textContent = value
+    document.querySelector("ul").appendChild(item)
 }
