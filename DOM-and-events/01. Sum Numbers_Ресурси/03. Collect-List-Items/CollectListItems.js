fd0618e0a367b67c050220522f4d8ee5a74282fa
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2023-07-15 14:42:46 +0200

    Add files via upload

--- DOM-and-events/01. Sum Numbers_Ресурси/03. Collect-List-Items/CollectListItems.js
+++ DOM-and-events/01. Sum Numbers_Ресурси/03. Collect-List-Items/CollectListItems.js
@@ -1,3 +1,8 @@
 function extractText() {
-    // TODO
+    const listItems = Array.from(document.querySelectorAll("li"));
+
+    const text = listItems.map((item) => item.textContent).join("\n");
+
+    document.querySelector("textarea").value = text;
+
 }
