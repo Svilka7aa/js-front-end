04ae977e678bd04247a2f4efecc28e48e38447f2
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2023-07-17 15:18:32 +0200

    Add files via upload

--- DOM-and-events-exercise/01. Subtraction_Ресурси/08. Furniture/solution.js
+++ DOM-and-events-exercise/01. Subtraction_Ресурси/08. Furniture/solution.js
@@ -1,4 +1,62 @@
-function solve() {
+function solve() { 
+  const button = document.querySelector("#exercise button");
+  button.addEventListener("click", parseFurnitureInput);
 
-  //TODO...
+  const buyButton = document.querySelector("#exercise button:nth-of-type(2)")
+  buyButton.addEventListener("click", buySelectedFurniture);
+}
+
+function buySelectedFurniture() {
+  const checkBoxes = Array.from(document.querySelectorAll("input[type='checkbox']:checked"));
+  checkBoxes.map(checkBox => {
+    const row = checkBox.parentElement.parentElement;
+    const name = row.querySelector("td:nth-of-type(2)").textContent;
+    const price = row.querySelector("td:nth-of-type(3)").textContent;
+    const decFactor = row.querySelector("td:nth-of-type(4)").textContent;
+    
+    return {name, price, decFactor}
+  })
+}
+
+function parseFurnitureInput () {
+  const input = JSON.parse(document.querySelector("#exercise textarea").value);
+  const tableBody = document.querySelector("tbody");
+
+  input.map(furniture => {
+    const row = document.createElement("tr");
+
+    
+
+    const nameCell = document.createElement("td");
+    nameCell.textContent = furniture.name;
+
+    const priceCell = document.createElement("td");
+    priceCell.textContent = furniture.price;
+
+    const decorationFactirCell = document.createElement("td");
+    decorationFactirCell.textContent = furniture.decFactor;
+
+
+    const checkCell = document.createElement("td");
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox"
+    checkCell.appendChild(checkBox)
+
+
+    row.appendChild(createImgCell(furniture.img));
+    row.appendChild(nameCell);
+    row.appendChild(priceCell);
+    row.appendChild(decorationFactirCell);
+    row.appendChild(checkCell)
+
+    return row;
+  }).forEach(row => tableBody.appendChild(row));
+}
+
+function createImgCell(src) {
+  const imageCell = document.createElement("td");
+    const image = document.createElement("img");
+    image.src = src;
+    imageCell.appendChild(image)
+    return imageCell
 }
